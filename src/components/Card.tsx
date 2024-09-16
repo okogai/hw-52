f0c1bf372311cf1f9e5b98923510922c9834e1dd
Oleg <evolrn89@gmail.com>
2024-09-16 12:36:28 +0600

    Created component Card

---
+++ src/components/Card.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type Suit = 'diams' | 'hearts' | 'clubs' | 'spades';
+type Rank = '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
+
+interface CardProps {
+    rank: Rank;
+    suit: Suit;
+}
+
+const suitSymbols: Record<Suit, string> = {
+    diams: '♦',
+    hearts: '♥',
+    clubs: '♣',
+    spades: '♠'
+};
+
+const Card: React.FC<CardProps> = ({ rank, suit }) => {
+    return (
+      <span className={`card rank-${rank} ${suit}`}>
+      <span className="rank">{rank}</span>
+      <span className="suit">{suitSymbols[suit]}</span>
+    </span>
+    );
+};
+
+export default Card;
